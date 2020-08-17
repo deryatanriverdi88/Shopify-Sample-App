23671e9c431acf2dcc0287ae16b8f0683e6646cd
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-08-17 00:18:28 -0400

    replaces TextStyle component and text with the EmptyState component and example text

--- Pages/index.js
+++ Pages/index.js
@@ -5,9 +5,18 @@ const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 const Index = () => (
     <Page>
         <Layout>
-            <TextStyle variation="positive">
-                Sample app using React and Next.js
-            </TextStyle>
+            <EmptyState
+              heading="Discount your products temporarily"
+              action={{
+                  content: 'Select products',
+                  onAction: () => console.log('clicked'),
+              }}
+              image={ img }
+            >
+                <p>
+                   Sample app using React and Next.js
+                </p>
+            </EmptyState>
         </Layout>
     </Page>
 );
