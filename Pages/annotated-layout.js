e076f4e7d9b228da96edaf669407d1de3216aaf2
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-08-17 00:29:22 -0400

    replaces the existing div with the Polaris Annotated Layout component, and includes the first annotated section with a card component

--- Pages/annotated-layout.js
+++ Pages/annotated-layout.js
@@ -4,7 +4,20 @@ class AnnotatedLayout extends React.Component {
     state = {};
 
     render() {
-        return <div> Annotated Layout</div>
+        return (
+            <Page>
+                <Layout>
+                    <Layout.AnnotatedSection
+                    title="Default discount"
+                    description="Add a product to Sample App, it will automatically be discounted."
+                    >
+                    <Card sectioned>
+                        <div>Card</div>
+                    </Card>
+                    </Layout.AnnotatedSection>
+                </Layout>
+            </Page>
+        )
     };
 };
 
